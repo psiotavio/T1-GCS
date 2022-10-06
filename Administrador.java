e34100dddd55673a39559e8987c4e9451c1b0f8c
Felipe Bisotto <felipebisotto2000@gmail.com>
2022-10-06 17:52:01 -0300

    Erro add arrumado

--- Administrador.java
+++ Administrador.java
@@ -8,35 +8,37 @@ public class Administrador extends Usuario {
         super(id, nome, tipo);
     }
 
-    public void addUsuario(){
-        boolean aux = false;
-        int id = 0;
-        do{ //VALIDACAO PARA NAO TER IDS IGUAIS 
-            System.out.println("Qual o id do usuario? ");
-            id = in.nextInt();
+            public void addUsuario() {
+                boolean aux = false;
+                int id;
 
-            for (Usuario lst : Sistema.lstUsuarios) {
-                if (lst.getId()!=id) {
-                    aux = true;
+                System.out.println("Qual o id do usuario? ");
+                id = in.nextInt();
+
+                for (Usuario lst : Sistema.lstUsuarios) {
+                    if (lst.getId() != id) {
+                        aux = true;
+                    }
                 }
+
+                if (aux == false) {
+                    System.out.println("Qual o nome do usuario? ");
+                    String nome = inS.nextLine();
+                    System.out.println("Qual o tipo do usuario? ");
+                    String tipo = inS.nextLine();
+
+                    if (tipo.equalsIgnoreCase("Paciente")) { // ADD AS INFOS NA LINKEDLIST
+                        Usuario u = new Paciente(id, nome, tipo);
+                        Sistema.lstUsuarios.add(u);
+                    } else if (tipo.equalsIgnoreCase("Medico")) {
+                        Usuario u = new Medico(id, nome, tipo);
+                        Sistema.lstUsuarios.add(u);
+                    } else if (tipo.equalsIgnoreCase("Administrador")) {
+                        Usuario u = new Administrador(id, nome, tipo);
+                        Sistema.lstUsuarios.add(u);
+                    }
+                }
+                else System.out.println("ERRO ID JÁ EXISTE!");
             }
-        } while(aux==false);
-
-        System.out.println("Qual o nome do usuario? ");
-        String nome = inS.nextLine();
-        System.out.println("Qual o tipo do usuario? ");
-        String tipo = inS.nextLine();
-
-        if(tipo.equalsIgnoreCase("Paciente")){ //ADD AS INFOS NA LINKEDLIST
-            Usuario u = new Paciente(id, nome, tipo);
-            Sistema.lstUsuarios.add(u);
-        } else if(tipo.equalsIgnoreCase("Medico")){
-            Usuario u = new Medico(id, nome, tipo);
-            Sistema.lstUsuarios.add(u);
-        } else if(tipo.equalsIgnoreCase("Administrador")){
-            Usuario u = new Administrador(id, nome, tipo);
-            Sistema.lstUsuarios.add(u);
-        }
-    }
 
 }
